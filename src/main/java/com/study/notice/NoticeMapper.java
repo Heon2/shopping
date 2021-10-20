d8a4830167e3eb4956344f4d549ebd41afbd8f7f
Heon2 <adlgjs@naver.com>
2021-10-20 11:27:28 +0900

    shopping notice

---
+++ src/main/java/com/study/notice/NoticeMapper.java
@@ -0,0 +1,25 @@
+package com.study.notice;
+
+import java.util.List;
+import java.util.Map;
+
+import com.study.contents.ContentsDTO;
+
+public interface NoticeMapper {
+
+	int total(Map map);
+
+	List<ContentsDTO> list(Map map);
+
+	int create(NoticeDTO dto);
+
+	void upCnt(int noticeno);
+
+	NoticeDTO read(int noticeno);
+
+	int passwd(Map map);
+
+	int update(NoticeDTO dto);
+
+	int delete(int noticeno);		
+}
